e6edb41d8c7d6a29d5743211e6206a608805f953
olovm <olov.mckie@ub.uu.se>
2019-02-08 13:57:25 +0000

    CORA-699 Enable write access to fedora

--- docker/ingestPlaces.sh
+++ docker/ingestPlaces.sh
@@ -3,5 +3,5 @@
 echo "Ingesting places"
 for FILE in `find import -type f`
 do
-  ./fedora38/client/bin/fedora-ingest.sh f $FILE info:fedora/fedora-system:ATOMZip-1.1 alvin-docker-fedora:8443 fedoraAdmin changeit https
+  ./fedora38/client/bin/fedora-ingest.sh f $FILE info:fedora/fedora-system:ATOMZip-1.1 localhost:8443 fedoraAdmin changeit https
 done
